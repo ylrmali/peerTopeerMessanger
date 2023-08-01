caf117fd7abb8305226d39e868d39299ccdfabb4
Ali Yıldırım <ylrmali1289@gmail.com>
2023-08-01 18:28:16 +0300

    edited for writing file

---
+++ file.py
@@ -0,0 +1,7 @@
+file = open('data.txt', 'r+')
+file.writelines('\nali');
+asd = file.readlines()
+print(asd)
+file.close()
+
+
--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 import socket
-
+import json
+from datetime import datetime
 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 key = b'fidauth123456789'
+ip = open('data.txt', 'a+')
+msg = open('msg.txt', 'a+')
+json_data = open('json_data.txt', 'a+')
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('192.168.0.17', 5555))
+serv.bind(('192.168.11.214', 5555))
 serv.listen(5)
 while True:
   conn, addr = serv.accept()
@@ -15,7 +19,26 @@ while True:
   while True:
     data = conn.recv(1024)
     if not data: break
-    print(data)
+    print(addr[0])
+    ''' create block in txt '''
+    block = {
+      '1' : {
+        'ip': addr[0],
+        'msg': [str(data)],
+        'time': str(datetime.now())
+      }
+    }
+
+    with open('json_data.txt', 'a+') as json_file:
+      json.dump(block, json_file)
+
+
+    ''' add ip in txt '''
+    ip.writelines(f'\n{addr[0]}')
+    ip.close()
+    ''' add message in txt '''
+    msg.writelines(f'\n{data}')
+    msg.close()
     # from_client += data.decode('utf8')
     # from_client += data
     print (data) 
